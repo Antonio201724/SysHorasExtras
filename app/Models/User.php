111b4d714e6c3167073eab6ec7531f760db69885
Antonio <jcpariata201724@gmail.com>
2020-11-16 21:45:43 -0430

    Cambios mas generales

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,21 +3,40 @@
 namespace ControlHorasExtras\PHP_MVC\Models;
 use ControlHorasExtras\PHP_MVC\Service;
 
-
 class User {
 
     private $em;
+    private $el;
 
     public function __construct(){
         $this->em = new Service();
     }   
 
-
     public function getAll(){
-        $query = "select * from users";
+        $query = "select * from he.usuario inner join he.tipo_usuario on he.usuario.tipo_usuario = he.tipo_usuario.id_tipo_usuario";
         $result = pg_exec($this->em->vinculo, $query);
         pg_close($this->em->vinculo);
         return pg_fetch_all($result);
-    }
-	
+    }	
+
+   public function Agregar(){
+        $query = "insert INTO he.usuario VALUES    ";
+        $result = pg_exec($this->em->vinculo, $query);
+        pg_close($this->em->vinculo);
+        return pg_fetch_all($result);
+    }	
+
+   public function Edit(){
+        $query = "UPDATE from he.usuario VALUES   ";
+        $result = pg_exec($this->em->vinculo, $query);
+        pg_close($this->em->vinculo);
+        return pg_fetch_all($result);
+    }	
+
+   public function Delete($id){
+        $query = "DELETE from he.usuario where $id = [id]";
+        $result = pg_exec($this->em->vinculo, $query);
+        pg_close($this->em->vinculo);
+        return pg_fetch_all($result);
+    }	
 }
---
+++ public/assets/js/ModalFormUser.js
@@ -0,0 +1,10 @@
+ $(document).ready(function () {
+
+
+       //    $("#FormUser").bind("submit", function (e) {
+
+       //     e.preventDefault(); {
+       //       console.log("Hola");
+       //     }
+       //   }
+       // });
